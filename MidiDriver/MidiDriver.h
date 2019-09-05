b9d9d1328d54fd8ceaa6325e366cb91eecad8340
Travis MacDonald <x2015khn@stfx.ca>
2019-09-05 18:54:25 -0300

    First commit

---
+++ MidiDriver/MidiDriver.h
@@ -0,0 +1,19 @@
+//
+//  MidiDriver.h
+//  MidiDriver
+//
+//  Created by IT Services Loaner on 2019-09-05.
+//  Copyright © 2019 Convergence Lab. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MidiDriver.
+FOUNDATION_EXPORT double MidiDriverVersionNumber;
+
+//! Project version string for MidiDriver.
+FOUNDATION_EXPORT const unsigned char MidiDriverVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MidiDriver/PublicHeader.h>
+
+
---
+++ MidiDriver/Speaker.swift
@@ -0,0 +1,9 @@
+//
+//  Speaker.swift
+//  MidiDriver
+//
+//  Created by Travis MacDonald on 2019-09-05.
+//  Copyright © 2019 Convergence Lab. All rights reserved.
+//
+
+import Foundation
